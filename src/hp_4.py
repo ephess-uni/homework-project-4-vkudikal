cf09b7223fcbef1f31de7584c5a60895b17f0190
vkudikal <164584629+vkudikal@users.noreply.github.com>
2024-04-29 18:05:15 -0500

    Update hp_4.py
    Updated version

--- src/hp_4.py
+++ src/hp_4.py
@@ -9,8 +9,8 @@ def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
 
-    ds_new = [datetime.strptime(old_, "%Y-%m-%d").strftime('%d %b %Y') for old_ in old_dates]
-    return ds_new
+    dates_new = [datetime.strptime(old_, "%Y-%m-%d").strftime('%d %b %Y') for old_ in old_dates]
+    return dates_new
 
 
 def date_range(start, n):
@@ -20,12 +20,12 @@ def date_range(start, n):
     if not isinstance(start, str) or not isinstance(n, int):
         raise TypeError()
 
-    return_outpt = []
+    return_outpt = [] # returning the empty list called return_outpt
 
-    dw = datetime.strptime(start, '%Y-%m-%d')
+    dew = datetime.strptime(start, '%Y-%m-%d')
 
     for i in range(n):
-        return_outpt.append(dw + timedelta(days=i))
+        return_outpt.append(dew + timedelta(days=i))
 
     return return_outpt
 
@@ -35,22 +35,22 @@ def add_date_range(values, start_date):
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
 
-    sc = date_range(start_date, len(values))
-    t = list(zip(sc, values))
-    return t
+    bc = date_range(start_date, len(values))
+    z = list(zip(bc, values)) #creating list called and using zipping function
+    return z
 
 
 def UtilityMethod(infile):
     headSet = ("book_uid,isbn_13,patron_id,date_checkout,date_due,date_returned".
-               split(','))
+               split(',')) #creating variable called headSet by using split function of ","
 
     with open(infile, 'r') as f:
-        removehdr = DictReader(f, fieldnames=headSet)
-        allrestrows = [row for row in removehdr]
+        rhdr = DictReader(f, fieldnames=headSet)
+        alltherestrows = [row for row in rhdr]
 
-        allrestrows.pop(0)
+        alltherestrows.pop(0)
 
-    return allrestrows
+    return alltherestrows
 
 
 def fees_report(infile, outfile):
@@ -59,23 +59,23 @@ def fees_report(infile, outfile):
 
     frmt = '%m/%d/%Y'
     rows = UtilityMethod(infile)
-    fees_ = defaultdict(float)
+    fees_of = defaultdict(float)
 
     for single_each_line in rows:
         patron = single_each_line['patron_id']
-        duedate = datetime.strptime(single_each_line['date_due'], frmt)
-        returnedDate = datetime.strptime(single_each_line['date_returned'], frmt)
-        ds = (returnedDate - duedate).days
-        fees_[patron] += 0.25 * ds if ds > 0 else 0.0
+        lastdate = datetime.strptime(single_each_line['date_due'], frmt)
+        returned_date = datetime.strptime(single_each_line['date_returned'], frmt)
+        dos = (returned_date - lastdate).days
+        fees_of[patron] += 0.25 * dos if dos > 0 else 0.0
 
     final_out = [
-        {'patron_id': pn, 'late_fees': f'{fs:0.2f}'} for pn, fs in fees_.items()
+        {'patron_id': pn, 'late_fees': f'{fs:0.2f}'} for pn, fs in fees_of.items()
     ]
 
-    with open(outfile, 'w') as wrt:
-        dcrr = DictWriter(wrt, ['patron_id', 'late_fees'])
-        dcrr.writeheader()
-        dcrr.writerows(final_out)
+    with open(outfile, 'w') as wrt: #opening the file using writing function
+        docrr = DictWriter(wrt, ['patron_id', 'late_fees'])
+        docrr.writeheader()
+        docrr.writerows(final_out)
 
 
 # The following main selection block will only run when you choose
